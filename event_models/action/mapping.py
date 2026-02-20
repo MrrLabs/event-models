2f4bd5cf3bac4267644559eb818cf993f88cd32f
jiri <jiri@mirrorlabs.co>
2026-02-20 12:10:08 +0100

    Reorder and adjust `action_exchange_id_create`/`action_exchange_id_update

--- event_models/action/mapping.py
+++ event_models/action/mapping.py
@@ -7,14 +7,13 @@ from event_models.exchange.exchange import EventExchange
 
 
 class ListingExchangeBaseMappingSchema(BaseModel):
-    action_exchange_id_create: PositiveInt | None = Field(gt=0, examples=[1])
-    action_exchange_id_update: PositiveInt | None = Field(gt=0, examples=[1])
     listing_id: PositiveInt = Field(gt=0, examples=[1])
     exchange: EventExchange
     full_sync: bool = Field(default=False)
 
 
 class ListingExchangeMappingRequestSchema(ListingExchangeBaseMappingSchema):
+    action_exchange_id_create: PositiveInt | None = Field(gt=0, examples=[1])
     inventory_id: PositiveInt = Field(gt=0, examples=[1])
     external_id: PositiveInt = Field(gt=0, examples=[1])
     valid_from: datetime.datetime = Field(examples=["2023-10-01T12:00:00"])
@@ -28,10 +27,10 @@ class ListingExchangeMappingRequestSchema(ListingExchangeBaseMappingSchema):
             "valid_from": self.valid_from,
             "full_sync": self.full_sync,
             "action_exchange_id_create": self.action_exchange_id_create,
-            "action_exchange_id_update": self.action_exchange_id_update,
         }
 
 
 class UpdateListingMappingRequestSchema(ListingExchangeBaseMappingSchema):
     inventory_id: PositiveInt = Field(gt=0, examples=[1])
     valid_to: datetime.datetime = Field(examples=["2023-10-01T12:00:00"])
+    action_exchange_id_update: PositiveInt | None = Field(gt=0, examples=[1])
